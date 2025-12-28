82b8876835c0cfc99420f524d88f1821b4904743
Rinarosen <rinarosen10@gmail.com>
2025-12-28 18:39:53 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function App() {
+  const motel = {
+    name: "Motel One Frankfurt-Römer",
+    address: "Berliner Str. 55, Frankfurt",
+  };
+
+  const places = [
+    {
+      category: "🍛 Indian & Vegetarian",
+      items: [
+        {
+          name: "Saravanaa Bhavan",
+          desc: "South Indian vegetarian, similar to Sarabejavan",
+          time: "12 min walk",
+        },
+        {
+          name: "Veer Savarkar",
+          desc: "Authentic Indian vegetarian",
+          time: "10 min walk",
+        },
